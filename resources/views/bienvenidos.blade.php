3eaafc701122c6383a1a2416ccb0bb232a9ff249
fcva-senati <fvigilioa@senati.pe>
2025-10-02 10:40:49 -0500

    Archivo vista de bienvenido

---
+++ resources/views/bienvenidos.blade.php
@@ -0,0 +1,2 @@
+
+<h1>Hola Soy Michael Jacson</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/saludos', function() {
+    return 'Hola amiguitos!';
+});
+
+Route::get('/bienvenidos', function() {
+    return view('bienvenidos');
+});
